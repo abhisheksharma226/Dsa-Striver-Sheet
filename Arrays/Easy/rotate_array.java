c2e2f62ca567a1e10bbf38e4fa85396999df24a7
abhisheksharma226 <avisheksharma2004@gmail.com>
2025-07-06 12:45:38 +0530

    rotate array by k anohter way

--- Arrays/Easy/rotate_array.java
+++ Arrays/Easy/rotate_array.java
@@ -5,9 +5,9 @@ public class rotate_array {
         int n = arr.length; 
         k = k % n; //Normalize K
 
-        reverse(arr, 0, n-1);
         reverse(arr, 0, k-1);
         reverse(arr, k, n-1);
+        reverse(arr, 0, n-1);
         
     }
 
@@ -22,8 +22,8 @@ public class rotate_array {
     }
 
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6,7};
-        rotate(arr, 3);
+        int arr[] = {1, 2, 3, 4, 5, 6};
+        rotate(arr, 2);
 
         for(int num: arr){
             System.out.print(num + " ");
