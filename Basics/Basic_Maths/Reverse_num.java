8a4e6317f4e05b6bc38dd127af69f6c0b2297624
abhisheksharma226 <avisheksharma2004@gmail.com>
2025-12-23 14:16:13 +0530

    Reverse Number

---
+++ Basics/Basic_Maths/Reverse_num.java
@@ -0,0 +1,15 @@
+package Basic_Maths;
+
+public class Reverse_num {
+    public static void main(String[] args) {
+        int num = 123456;
+        int rev = 0;
+
+        while ( num > 0){
+            int lastDigit = num % 10;
+            rev = rev * 10 + lastDigit;
+            num = num / 10;
+        }
+        System.out.print(rev);
+    }
+}
