2a2649d207a6e5132e774128a8b3da1f9479a905
abhisheksharma226 <avisheksharma2004@gmail.com>
2025-12-23 12:58:31 +0530

    Factorial

---
+++ Basics/Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int n = sc.nextInt();
+        int fact = 1;
+
+        while(n > 0){
+            fact *= n; 
+            n--;
+        }
+
+        System.out.print("Factorial of " + n +  " is " + fact);
+    }
+}
+
