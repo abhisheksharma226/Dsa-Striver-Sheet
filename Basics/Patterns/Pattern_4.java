975fa75ff35383feeee9e6c9ead8d6fd1ba8d33f
abhisheksharma226 <avisheksharma2004@gmail.com>
2025-12-23 13:32:23 +0530

    Pattern 4

---
+++ Basics/Patterns/Pattern_4.java
@@ -0,0 +1,14 @@
+package Patterns;
+
+public class Pattern_4 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i = 0; i < n; i++){
+            for(int j = n; j > i; j--){
+                System.out.print((n - j + 1) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
