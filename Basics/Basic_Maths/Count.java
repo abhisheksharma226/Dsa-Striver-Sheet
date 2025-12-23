8b83029d578b2ba0308e0352c96ce0b2b0b32db7
abhisheksharma226 <avisheksharma2004@gmail.com>
2025-12-23 13:48:53 +0530

    Count Number

---
+++ Basics/Basic_Maths/Count.java
@@ -0,0 +1,15 @@
+package Basic_Maths;
+
+public class Count {
+    public static void main(String[] args) {
+        int n = 12498;
+        int count = 0;
+
+        while ( n > 0){
+            n = n / 10;
+            count ++;
+        }
+
+        System.out.print(count);
+    }
+}
