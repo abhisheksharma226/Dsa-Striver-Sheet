26908558ec6e89df342b9b4be0c050f79d4a88e5
abhisheksharma226 <avisheksharma2004@gmail.com>
2025-12-25 17:24:41 +0530

    Reverse an array

--- Basics/Recursion/Main.java
+++ Basics/Recursion/Main.java
@@ -19,19 +19,41 @@ class Solution {
         return fact;
         
     }
+
+    //Reverse a given array
+    public int[] reverseArray(int arr[]){
+        int n = arr.length;
+        int[] ans = new int[n];
+
+        for(int i = 0; i<n; i++){
+            ans[i] = arr[n - 1 - i];
+        }
+        return ans;
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
         Solution sc = new Solution();
-        int n = 15;
-        int result = sc.sumOfNaturalNumbers(n);
-        System.out.println("Sum of natural numbers from 1 to " + n + " is: " + result);
+
+        // //Sum of Natural Number
+        // int n = 15;
+        // int result = sc.sumOfNaturalNumbers(n);
+        // System.out.println("Sum of natural numbers from 1 to " + n + " is: " + result);
 
 
-        //factorial
-        int fact = 5;
-        int factorial = sc.factorial(fact);
-        System.out.print("factorial is : " + factorial);
+        // //factorial
+        // int fact = 5;
+        // int factorial = sc.factorial(fact);
+        // System.out.print("factorial is : " + factorial);
+
+        //Reverse a given array
+        int arr[] = {1,2,3,4,5};
+        int[] reversArray = sc.reverseArray(arr);
+        
+        for(int i = 0; i < reversArray.length; i++){
+            System.out.print(reversArray[i] + " ");
+        }
+        System.out.println();
     }
 }
