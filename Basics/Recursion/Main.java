200f93b7da308938e678f10b257495f92adade93
abhisheksharma226 <avisheksharma2004@gmail.com>
2025-12-25 17:13:23 +0530

    Factorial

--- Basics/Recursion/Main.java
+++ Basics/Recursion/Main.java
@@ -1,6 +1,7 @@
 // package Basics.Recursion;
 
 class Solution {
+    //Sum of Natual Number
     public int sumOfNaturalNumbers(int N) {
         int sum = 0;
         for (int i = 1; i <= N; i++) {
@@ -9,7 +10,15 @@ class Solution {
         return sum;
     }
 
-    
+    //Factorial
+    public int factorial(int n ){
+        int fact = 1;
+        for(int i = 1; i<=n ; i++){
+            fact *= i;
+        }
+        return fact;
+        
+    }
 }
 
 public class Main {
@@ -18,5 +27,11 @@ public class Main {
         int n = 15;
         int result = sc.sumOfNaturalNumbers(n);
         System.out.println("Sum of natural numbers from 1 to " + n + " is: " + result);
+
+
+        //factorial
+        int fact = 5;
+        int factorial = sc.factorial(fact);
+        System.out.print("factorial is : " + factorial);
     }
 }
